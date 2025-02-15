e052fa9250a12f7f122e20004048cd65099d4abc
iamvevaar <gautam15suthar@gmail.com>
2025-02-15 14:21:52 +0530

    Tailwind v3 added

--- src/renderer.jsx
+++ src/renderer.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import ReactDOM  from 'react-dom/client'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1>Hello</h1>
+    <h1 className='font-bold text-fuchsia-500'>Hello</h1>
   </StrictMode>,
 )
